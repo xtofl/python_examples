30b6f1ede5101827e8f9a5c09b3ea25169dfa4dd
xtofl <kristoffel.pirard@gmail.com>
2020-02-26 18:52:25 +0100

    Show the problem with lists vs tuples

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # python_examples
+
+Showing some python snippets.
---
+++ test_mutable_vs_immutable.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+__doc__ = """
+It came to my attention that some people prefer working
+with lists over tuples, because... you can sort a list,
+you can append to a list, ...
+
+I think lists, as any mutable data structures, are more
+hard to use than you would think.
+
+Compare the two variations of the same algorithm to produce
+a triangle of numbers.  The test with lists fails, while
+the test with tuples succeeds.
+
+Can you spot the error?
+"""
+
+def tree_of_lists(n):
+    result = []
+    line = []
+    for i in range(n):
+        result.append(line)
+        line.append(i)
+    return result
+
+def tree_of_tuples(n):
+    result = tuple()
+    line = tuple()
+    for i in range(n):
+        result = result + (line,)
+        line = line + (i,)
+    return result
+
+def test_lists():
+    assert tree_of_lists(3) == [[], [0], [0, 1]]
+
+def test_tuples():
+    assert tree_of_tuples(3) == (tuple(), (0,), (0, 1))
+
